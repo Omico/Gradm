010c0f779d196bfea88a53d2af0192389a1fb2d5
Omico <me@omico.icu>
2022-03-04 23:28:16 -0800

    example: Use Gradm plugin from mavenLocal()

--- example/settings.gradle.kts
+++ example/settings.gradle.kts
@@ -3,12 +3,14 @@
 rootProject.name = "gradm-example"
 
 pluginManagement {
-    includeBuild("..")
     repositories {
         gradlePluginPortal()
         mavenCentral()
         mavenLocal()
         maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
+    }
+    plugins {
+        id("me.omico.gradm") version "1.0.0-SNAPSHOT"
     }
 }
 
