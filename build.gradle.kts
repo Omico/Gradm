35a404956f30b6833404bb4c55372d498daa609c
Omico <me@omico.icu>
2022-06-04 09:10:01 -0700

    release: 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val kotlinDslVersion = "2.1.7" // From embedded Kotlin
 
 allprojects {
     group = "me.omico.gradm"
-    version = "1.8.0-SNAPSHOT"
+    version = "2.0.0"
     configureDependencyUpdates(
         pinnedGroups = mapOf(
             "org.gradle.kotlin.kotlin-dsl" to kotlinDslVersion,
