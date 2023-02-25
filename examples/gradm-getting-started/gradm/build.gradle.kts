7adcad12111fa9c170f2ed8128cbf1162858ce85
Omico <me@omico.icu>
2023-02-25 00:27:54 -0800

    example: Always refresh Gradm snapshot plugin

--- examples/gradm-getting-started/gradm/build.gradle.kts
+++ examples/gradm-getting-started/gradm/build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript {
+    configurations.all {
+        resolutionStrategy.cacheChangingModulesFor(0, TimeUnit.SECONDS)
+    }
+}
+
 plugins {
     `kotlin-dsl`
     id("me.omico.gradm") version "3.1.0-SNAPSHOT"
