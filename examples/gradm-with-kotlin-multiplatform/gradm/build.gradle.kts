f8ad8ff96759f20b07f47f871cdb39b9d0c538c3
Omico <me@omico.icu>
2023-05-07 15:51:06 -0700

    examples: Add gradm with kotlin multiplatform example (#61)

---
+++ examples/gradm-with-kotlin-multiplatform/gradm/build.gradle.kts
@@ -0,0 +1,23 @@
+buildscript {
+    configurations.all {
+        resolutionStrategy.cacheChangingModulesFor(0, TimeUnit.SECONDS)
+    }
+}
+
+plugins {
+    `kotlin-dsl`
+    id("me.omico.gradm") version "3.3.0-SNAPSHOT"
+}
+
+repositories {
+    mavenCentral()
+}
+
+gradm {
+    pluginId = "gradm"
+    debug = true
+    experimental {
+        kotlinMultiplatformSupport = true
+        kotlinMultiplatformIgnoredExtensions = listOf("androidx")
+    }
+}
