028f06a894a518f2c686d768935cb53fa7eb81bb
Omico <me@omico.icu>
2023-07-23 22:06:37 -0700

    Update to Elucidator 0.12.0

--- examples/gradm-getting-started/gradm/settings.gradle.kts
+++ examples/gradm-getting-started/gradm/settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
     repositories {
         mavenLocal()
         maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
+        maven(url = "https://maven.omico.me")
         mavenCentral()
         gradlePluginPortal()
     }
