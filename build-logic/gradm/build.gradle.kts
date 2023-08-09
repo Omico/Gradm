47a6a706115a9ebbe4254aac4f33013a4df1a8bc
Omico <me@omico.icu>
2023-08-09 11:17:47 -0700

    Adapt new Gradm

--- build-logic/gradm/build.gradle.kts
+++ build-logic/gradm/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `kotlin-dsl`
-    id("me.omico.gradm") version "3.3.3"
+    id("me.omico.gradm") version "4.0.0-beta02"
+    id("me.omico.gradm.integration.github") version "4.0.0-beta02"
 }
 
 repositories {
@@ -10,7 +11,4 @@ repositories {
 gradm {
     pluginId = "gradm.gradm"
     debug = true
-    integrations {
-        register("github")
-    }
 }
