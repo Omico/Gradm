affbc5eee6a1693f9da31e60918531ad320b97eb
Omico <me@omico.icu>
2023-08-09 11:02:52 -0700

    release: 4.0.0-beta02

--- examples/gradm-getting-started/gradm/build.gradle.kts
+++ examples/gradm-getting-started/gradm/build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
 
 plugins {
     `kotlin-dsl`
-    id("me.omico.gradm") version "4.0.0-SNAPSHOT"
-    id("me.omico.gradm.integration.github") version "4.0.0-SNAPSHOT"
+    id("me.omico.gradm") version "4.0.0-beta02"
+    id("me.omico.gradm.integration.github") version "4.0.0-beta02"
 }
 
 repositories {
