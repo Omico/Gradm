81bc076458671e61ed4d5536698c4f11d2e99503
Omico <me@omico.icu>
2023-12-06 20:54:13 -0800

    Update dependencies

--- examples/gradm-getting-started/settings.gradle.kts
+++ examples/gradm-getting-started/settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version "3.15.1"
+    id("com.gradle.enterprise") version "3.16"
     id("gradm") // configured by GradmExtension
 }
 
